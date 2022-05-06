c4af7e080406e4e0a6c9908b53b77669b412fbbe
Ishaan Walecha <64886059+ishaanw007@users.noreply.github.com>
2022-05-06 15:19:46 +0530

    doneallchangesrequested3

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
-import { jsx, Heading, Box, Container, Text } from 'theme-ui';
-import { Link } from 'components/link';
-import menuItems from './footer.data';
+import { jsx, Heading, Box, Container, Text } from "theme-ui";
+import { Link } from "components/link";
+import menuItems from "./footer.data";
 export default function Footer() {
   return (
     <footer sx={styles.footer}>
@@ -27,7 +27,7 @@ export default function Footer() {
       <Text sx={styles.footer.copyright}>
         All right reserved - Design & Developed by
         <Link path="https://extensionerp.com/" target="_blank">
-        Extension Technologies Pvt. Ltd.
+          Extension Technologies Pvt. Ltd.
         </Link>
       </Text>
     </footer>
@@ -37,64 +37,81 @@ export default function Footer() {
 const styles = {
   footer: {
     container: {
-      width: '100%',
-      alignItems: 'stretch',
+      // display: "flex",
+      // justifyContent: "space-between",
+      width: "100%",
+      alignItems: "center",
+      // backgroundColor: "yellow",
+
+      // ml: "22%",
     },
     footerTopArea: {
-      borderTop: '1px solid',
-      borderTopColor: 'border_color',
-      display: 'flex',
-      flexWrap: 'wrap',
-      pt: [7, null, 8],
-      pb: ['10px', null, null, '20px'],
-      px: [0, null, null, null, 4],
+      // borderTop: "1px solid",
+      borderTopColor: "border_color",
+      display: "flex",
+      // justifyContent: "space-between",
+      // alignItems: "center",
+      // backgroundColor: "red",
+      width: "100%",
+      flexWrap: "wrap",
+
+      // pt: [7, null, 8],
+      // p: "200px",
+      // pb: ["10px", null, null, "20px"],
+      // px: [0, null, null, null, 4],
     },
     menus: {
-      width: ['50%', null, null, '25%'],
-      display: 'flex',
-      flexDirection: 'column',
-      mb: ['40px', null, null, '60px'],
+      width: "25%",
+      display: "flex",
+      flexDirection: "column",
+      // justifyContent: "center",
+      // backgroundColor: "green",
+      alignItems: "center",
+      // m: "5px",
     },
 
     heading: {
-      fontSize: [3, null, null, 4],
-      color: 'text_secondary',
-      fontWeight: '500',
-      mb: [3, 4, 5, null, 6],
-      lineHeight: '1.35',
+      fontSize: 3,
+      color: "text_secondary",
+      fontWeight: "500",
+      mb: "10px",
+      lineHeight: "1.35",
+      // backgroundColor: "yellow",
+      mr: "20px",
+      alignItems: "flex-start",
     },
 
     link: {
-      fontSize: ['14px', null, 1],
-      color: 'text',
-      fontWeight: 'body',
+      fontSize: ["14px", null, 1],
+      color: "text",
+      fontWeight: "body",
       mb: 2,
-      cursor: 'pointer',
-      transition: 'all 0.35s',
-      display: 'block',
-      textDecoration: 'none',
+      cursor: "pointer",
+      transition: "all 0.35s",
+      display: "block",
+      textDecoration: "none",
       lineHeight: [1.5, null, null, 1.6, 1.8],
-      ':hover': {
-        color: 'primary',
+      ":hover": {
+        color: "primary",
       },
-      ':last-child': {
-        mb: '0px',
+      ":last-child": {
+        mb: "0px",
       },
     },
     copyright: {
-      fontSize: ['14px', null, 1],
-      width: '100%',
-      textAlign: 'center',
-      p: ['20px 20px'],
-      backgroundColor: '#FCFDFE',
-      color: 'text',
+      fontSize: ["14px", null, 1],
+      width: "100%",
+      textAlign: "center",
+      p: ["20px 20px"],
+      backgroundColor: "#FCFDFE",
+      color: "text",
       a: {
-        textDecoration: 'none',
-        color: 'inherit',
+        textDecoration: "none",
+        color: "inherit",
         pl: 1,
-        transition: 'all 0.25s',
-        '&:hover': {
-          color: 'primary',
+        transition: "all 0.25s",
+        "&:hover": {
+          color: "primary",
         },
       },
     },
