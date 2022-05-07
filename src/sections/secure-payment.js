d8bcfaf39e3ef0b0b436cc681fa3f1743b2a7492
Ishaan Walecha <64886059+ishaanw007@users.noreply.github.com>
2022-05-07 17:23:24 +0530

    doneallchangesrequested10

--- src/sections/secure-payment.js
+++ src/sections/secure-payment.js
@@ -1,53 +1,178 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 import { Container, Box } from "theme-ui";
+import {
+  Label,
+  Input,
+  Select,
+  Textarea,
+  Radio,
+  Checkbox,
+  Slider,
+  Flex,
+  Button,
+  Heading,
+  Text,
+} from "theme-ui";
 import TextFeature from "components/text-feature";
 import Image from "components/image";
 
 import PaymentThumb from "assets/paymentThumb.png";
 import PaymentPattern from "assets/payment-pattern.png";
-
+const heading = {
+  heading: "Contact Us",
+};
 const data = {
   subTitle: "",
-  title: "Explore our standard App for free:",
-  description:
-    "Discover how Extension Technologies can help you sell better. Find out how you can control your operations from one centralised platform with Extension’s OMS app. Download and view our demo application in the playstore for free! ",
-  btnName: "Learn More",
-  btnURL: "#",
+  title: "Contact ",
+  description: "+91 - 9871965334",
+  // btnName: "Learn More",
+  // btnURL: "#",
 };
 const data2 = {
-  subTitle: "",
-  title: "Want a customised demo for app?",
-  description:
-    "Get a personalised solution and see for yourself what we’re all about",
-  btnName: "Learn More",
-  btnURL: "#",
+  // subTitle: "",
+  title: "Based in",
+  contents: (
+    <div>
+      G11-12,SRS TOWER<br></br>
+      Sector 31<br></br>
+      Faridabad<br></br>
+      Haryana (121003)
+    </div>
+  ),
 };
+const data3 = {
+  title: "Get in Touch",
+};
+// const data = {
+//   subTitle: "",
+//   title: "Explore our standard App for free:",
+//   description:
+//     "Discover how Extension Technologies can help you sell better. Find out how you can control your operations from one centralised platform with Extension’s OMS app. Download and view our demo application in the playstore for free! ",
+//   btnName: "Learn More",
+//   btnURL: "#",
+// };
+// const data2 = {
+//   subTitle: "",
+//   title: "Want a customised demo for app?",
+//   description:
+//     "Get a personalised solution and see for yourself what we’re all about",
+//   btnName: "Learn More",
+//   btnURL: "#",
+// };
 
 export default function SecurePayment() {
   return (
     <section sx={{ variant: "section.securePayment" }}>
       <Box sx={styles.bgOverlay} />
-      <Container sx={styles.containerBox}>
-        <Box sx={styles.contentBox}>
-          {/* <Image src={PaymentThumb} alt={data.title} /> */}
+      <Container sx={styles.containerBox2}>
+        <Container sx={styles.containerBox}>
+          <Heading sx={styles.title}>Contact Us</Heading>
+          <Box sx={styles.contentBox}>
+            <Heading sx={styles.contentBox.title}>{data.title}</Heading>
+            <Text sx={styles.contentBox.desc}>{data.description}</Text>
+            {/* <Image src={PaymentThumb} alt={data.title} /> */}
+            {/* <TextFeature */}
+            {/* // subTitle={data.subTitle}
+              // title={data.title}
+              description={data.description}
+              // btnName={data.btnName}
+              // btnURL={data.btnURL}
+            /> */}
+          </Box>
+          <Box sx={styles.contentBox}>
+            <Heading sx={styles.contentBox.title}>{data2.title}</Heading>
+            <Text sx={styles.contentBox.desc}>{data2.contents}</Text>
+            {/* // subTitle={data2.subTitle}
+              // title={data2.title}
+             
+              // btnNam={data2.btnName}
+              // btnURL={data2.btnURL} */}
+          </Box>
+        </Container>
+
+        <Container sx={styles.containerBox3}>
           <TextFeature
-            subTitle={data.subTitle}
-            title={data.title}
-            description={data.description}
-            btnName={data.btnName}
-            btnURL={data.btnURL}
+            // subTitle={data2.subTitle}
+            title={data3.title}
+            // description={data2.contents}
+            // btnNam={data2.btnName}
+            // btnURL={data2.btnURL}
           />
-        </Box>
-        <Box sx={styles.contentBox}>
-          <TextFeature
-            subTitle={data2.subTitle}
-            title={data2.title}
-            description={data2.description}
-            btnName={data2.btnName}
-            btnURL={data2.btnURL}
-          />
-        </Box>
+          <Box as="form" onSubmit={(e) => e.preventDefault()}>
+            {/* <Label htmlFor="username">Username</Label> */}
+            <Input
+              name="name"
+              id="name"
+              // label="Full Name"
+              placeholder="Full Name"
+              mb={3}
+            />
+            {/* <Label htmlFor="password">Password</Label> */}
+            <Input name="email" id="email" placeholder="Email" mb={3} />
+            <Input
+              name="mobile"
+              id="mobile"
+              placeholder="Mobile Number"
+              mb={3}
+            />
+            <Input
+              name="organization"
+              id="organization"
+              placeholder="Organization Name"
+              mb={3}
+            />
+            {/* <Box>
+            <Label mb={3}>
+            <Checkbox />
+            Remember me
+            </Label>
+          </Box> */}
+            {/* <Label htmlFor="sound">Sound</Label> */}
+            <Select
+              name="source"
+              id="source"
+              placeholder="Select Source"
+              mb={3}
+            >
+              <option value="" disabled selected hidden>
+                Select Source
+              </option>
+              <option>Beep</option>
+              <option>Boop</option>
+              <option>Blip</option>
+            </Select>
+            <Select
+              name="designation"
+              id="designation"
+              defautValue="Boop"
+              mb={3}
+            >
+              <option value="" disabled selected hidden>
+                Select Designation
+              </option>
+              <option>Beep</option>
+              <option>Boop</option>
+              <option>Blip</option>
+            </Select>
+            {/* <Label htmlFor="comment">Comment</Label> */}
+            {/* <Textarea name="comment" id="comment" rows={6} mb={3} /> */}
+            {/* <Flex mb={3}>
+            <Label>
+            <Radio name="letter" /> Alpha
+            </Label>
+            <Label>
+            <Radio name="letter" /> Bravo
+            </Label>
+            <Label>
+            <Radio name="letter" /> Charlie
+            </Label>
+          </Flex> */}
+            {/* <Label>Slider</Label> */}
+            {/* <Slider mb={3} /> */}
+            <Button sx={styles.save}>Save</Button>
+          </Box>
+        </Container>
       </Container>
     </section>
   );
@@ -55,7 +180,9 @@ export default function SecurePayment() {
 
 const styles = {
   bgOverlay: {
-    position: "absolute",
+    display: "flex",
+    backgroundColor: "red",
+    // position: "absolute",
     top: 0,
     right: 0,
     height: "100%",
@@ -75,26 +202,76 @@ const styles = {
     backgroundPosition: "top left",
     backgroundSize: "cover",
   },
-  containerBox: {
+  containerBox2: {
     display: "flex",
-    alignItems: "flex-start",
-    justifyContent: "space-between",
-    flexWrap: ["wrap", null, null, "nowrap"],
+    borderRadius: "15px",
+    backgroundColor: "#1F3E76",
+    "@media screen and (max-width:1220px)": {
+      display: "block",
+      p: "10px",
+    },
+    p: "20px",
+    // alignItems: "flex-start",
+    // justifyContent: "space-between",
+    // flexWrap: ["wrap", null, null, "nowrap"],
   },
-  thumbnail: {
-    mb: -2,
-    ml: [-4, "-40px", null, null, 0],
-    order: [2, null, null, 0],
-    pr: [6, null, 7, 0],
+  save: {
+    // alignItems: "center",
+    // justifyContent: "cen0ter",
+    // position: "absolute",
+    // bottom: "10%",
+    // left: "50%",
   },
+  containerBox3: {
+    // display: "flex",
+    m: "30px",
+    backgroundColor: "white",
+    borderRadius: "15px",
+    alignItems: "center",
+    p: "20px",
+    "@media screen and (max-width:1220px)": {
+      display: "block",
+      m: "0px",
+    },
+
+    // alignItems: "flex-start",
+    justifyContent: "center",
+    // flexWrap: ["wrap", null, null, "nowrap"],
+  },
+
   contentBox: {
-    width: ["100%", 420, 480, 380, 500, 570],
-    mx: "auto",
-    flexShrink: 0,
-    textAlign: ["center", null, null, "left"],
-    pt: [0, null, null, 4, "50px", null, 4, "80px"],
-    pl: [0, null, null, 40, "90px"],
-    pb: [7, null, null, 9],
-    pr: [0, null, null, null, null, 6],
+    // backgroundColor: "black",
+    // color: "white",
+    p: "10px",
+    title: {
+      color: "white",
+      fontSize: "45px",
+    },
+    desc: {
+      color: "white",
+    },
   },
+  title: {
+    color: "white",
+    fontSize: "65px",
+  },
+  containerBox: {
+    p: "20px",
+  },
+  // thumbnail: {
+  //   mb: -2,
+  //   ml: [-4, "-40px", null, null, 0],
+  //   order: [2, null, null, 0],
+  //   pr: [6, null, 7, 0],
+  // },
+  // contentBox: {
+  //   width: ["100%", 420, 480, 380, 500, 570],
+  //   mx: "auto",
+  //   flexShrink: 0,
+  //   textAlign: ["center", null, null, "left"],
+  //   pt: [0, null, null, 4, "50px", null, 4, "80px"],
+  //   pl: [0, null, null, 40, "90px"],
+  //   pb: [7, null, null, 9],
+  //   pr: [0, null, null, null, null, 6],
+  // },
 };
