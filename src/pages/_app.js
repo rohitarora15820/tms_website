2e1feb2a5f4d96cbe4e1823b5484fd1d5a893511
Ishaan Walecha <64886059+ishaanw007@users.noreply.github.com>
2022-05-07 10:53:36 +0530

    doneallchangesrequested7

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,19 @@
-import { useEffect } from 'react';
-import Router from 'next/router';
-import { initGA, logPageView } from 'analytics';
+import { useEffect } from "react";
+import Router from "next/router";
+import { initGA, logPageView } from "analytics";
 // Load DM Sans typeface
-import 'typeface-dm-sans';
+import "typeface-dm-sans";
 
 // Load other package css file
-import 'react-multi-carousel/lib/styles.css';
-import 'react-modal-video/css/modal-video.min.css';
-import 'rc-drawer/assets/index.css';
+import "react-multi-carousel/lib/styles.css";
+import "react-modal-video/css/modal-video.min.css";
+import "rc-drawer/assets/index.css";
 
 export default function CustomApp({ Component, pageProps }) {
   useEffect(() => {
     initGA();
     logPageView();
-    Router.events.on('routeChangeComplete', logPageView);
+    Router.events.on("routeChangeComplete", logPageView);
   }, []);
 
   return <Component {...pageProps} />;
